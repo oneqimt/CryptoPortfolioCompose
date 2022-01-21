8e24c5becc0dc62cd8456b428b4c971fceb3bd15
oneqimt <oneqimt@gmail.com>
2022-01-21 04:59:58 -0700

    adding CircularProgressBar

---
+++ app/src/main/java/com/imtmobileapps/cryptocompose/components/CircularProgressBar.kt
@@ -0,0 +1,25 @@
+package com.imtmobileapps.cryptocompose.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularProgressBar(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(50.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colors.primary
+        )
+    }
+}
