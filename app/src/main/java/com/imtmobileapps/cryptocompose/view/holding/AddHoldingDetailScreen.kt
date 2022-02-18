7d3157e1e785890504e5d42444dfb1c77967e1b6
oneqimt <oneqimt@gmail.com>
2022-02-17 17:07:49 -0700

    Work in progress, adding AddHoldingDetailScreen.kt design view.

--- app/src/main/java/com/imtmobileapps/cryptocompose/view/holding/AddHoldingDetailScreen.kt
+++ app/src/main/java/com/imtmobileapps/cryptocompose/view/holding/AddHoldingDetailScreen.kt
@@ -1,33 +1,38 @@
 package com.imtmobileapps.cryptocompose.view.holding
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.imtmobileapps.cryptocompose.R
 import com.imtmobileapps.cryptocompose.event.UIEvent
-import com.imtmobileapps.cryptocompose.ui.theme.coinNameTextColor
-import com.imtmobileapps.cryptocompose.ui.theme.topAppBarBackgroundColor
-import com.imtmobileapps.cryptocompose.ui.theme.topAppBarContentColor
+import com.imtmobileapps.cryptocompose.ui.theme.*
 import com.imtmobileapps.cryptocompose.viewmodel.ManageHoldingsViewModel
 import kotlinx.coroutines.flow.collect
 
 @Composable
 fun AddHoldingDetailScreen(
     viewModel: ManageHoldingsViewModel,
-    onPopBackStack: () -> Unit
+    onPopBackStack: () -> Unit,
 ) {
 
     BackHandler {
         onPopBackStack()
     }
+
+    val selectedCoin = viewModel.selectedCoin.collectAsState()
 
     LaunchedEffect(key1 = true) {
         viewModel.uiEvent.collect { event ->
@@ -42,15 +47,17 @@ fun AddHoldingDetailScreen(
         topBar = {
             TopAppBar(
                 navigationIcon = {
-                    IconButton(onClick = {onPopBackStack()}) {
+                    IconButton(onClick = { onPopBackStack() }) {
                         Icon(Icons.Filled.ArrowBack, "backIcon")
                     }
                 },
                 title = {
-                    Text(
-                        text = "ADD NAME OF COIN",
-                        color = MaterialTheme.colors.topAppBarContentColor
-                    )
+                    selectedCoin.value?.coinName?.let {
+                        Text(
+                            text = it,
+                            color = MaterialTheme.colors.topAppBarContentColor
+                        )
+                    }
                 },
                 backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
 
@@ -58,22 +65,81 @@ fun AddHoldingDetailScreen(
         },
 
         content = {
-            Column(
-                modifier = Modifier.padding(30.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(2.dp),
+                elevation = 2.dp,
+                border = BorderStroke(0.3.dp, MaterialTheme.colors.cardBorderColor),
+                backgroundColor = MaterialTheme.colors.cardBackgroundColor,
+                shape = RoundedCornerShape(corner = CornerSize(6.dp))
             ) {
-                //CircularProgressBar()
-                Text(
-                    modifier = Modifier.padding(10.dp, 10.dp),
-                    text = "ADD HOLDING DETAIL VIEW",
-                    color = MaterialTheme.colors.coinNameTextColor,
-                    style = MaterialTheme.typography.subtitle1,
-                )
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier.padding(10.dp, 20.dp)
+
+                    ) {
+                        selectedCoin.value?.coinName?.let { coinName ->
+                            Text(
+                                modifier = Modifier.padding(2.dp),
+                                text = coinName,
+                                color = MaterialTheme.colors.coinNameTextColor,
+                                style = MaterialTheme.typography.h6,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.padding(10.dp)
+
+                    ) {
+                        selectedCoin.value?.coinSymbol?.let { coinSymbol ->
+                            Text(
+                                modifier = Modifier.padding(2.dp),
+                                text = coinSymbol,
+                                color = MaterialTheme.colors.coinNameTextColor,
+                                style = MaterialTheme.typography.h6,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        }
+                    }
+                } // end 1st row
+                //Spacer(Modifier.height(40.dp))
+
+                Column(modifier = Modifier.padding(10.dp, 70.dp)) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(10.dp)
+
+                        ) {
+                            selectedCoin.value?.currentPrice?.let { currentPrice ->
+                                Text(
+                                    modifier = Modifier.padding(2.dp),
+                                    text = stringResource(id = R.string.current_price_add_holding,
+                                        currentPrice.toPlainString()),
+                                    color = MaterialTheme.colors.coinNameTextColor,
+                                    style = MaterialTheme.typography.h6,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
 
     )
-
 }
